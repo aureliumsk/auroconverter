9e684e46b8393f35cfe88e3e51835a66e78aa32d
JustLoveC1de <ijustlovecode@gmail.com>
2024-08-31 17:29:49 +0800

    Used rich in a different way.

--- auroconverter/__init__.py
+++ auroconverter/__init__.py
@@ -1,7 +1,7 @@
 from .conv import File, spinning_progress, process_image
 from .caching import FileRelatedCache
 from rich.table import Table
-from rich import print as rprint
+from rich.console import Console
 from typing import List, Annotated
 from PIL import Image, ImageSequence
 from concurrent.futures import ProcessPoolExecutor, Future
@@ -15,6 +15,9 @@ import io
 
 app: typer.Typer = typer.Typer()
 cache: FileRelatedCache = FileRelatedCache(Path("__auroconv__"))
+console: Console = Console()
+
+
 
 def avgcol(img: np.ndarray, color: bool = True):
     avgcol: np.ndarray | np.floating = np.mean(img, axis=(0, 1))
@@ -66,7 +69,7 @@ def info(files: List[File]) -> None:
             size: tuple[int, int] = im.size
             table.add_row(file.name, im.format, f"{size[0]}x{size[1]}", im.mode)
 
-    rprint(table)
+    console.print(table)
 
 
 
@@ -75,24 +78,23 @@ def info(files: List[File]) -> None:
 def ansi(file: File, 
          cols: Annotated[int, typer.Option("--cols", "-c", help="Count of characters in one row.")] = 80, 
          scale: Annotated[float, typer.Option("--scale", "-s", help="Font size.")] = 0.43, 
-         char: Annotated[str, typer.Option("--char", help="Character, which will be used to display tiles.")] = "@", 
+         char: Annotated[str, typer.Option(help="Character, which will be used to display tiles.")] = "@", 
          anim: Annotated[bool, typer.Option("--anim", "-a", help="Display image as animation.")] = False,
-         color: Annotated[bool, typer.Option("--color", help="Use all kinds of colors.")] = True, 
-         caching: Annotated[bool, typer.Option("--caching", help="Use cached results.")] = True) -> None:
+         color: Annotated[bool, typer.Option(help="Use all kinds of colors.")] = True, 
+         caching: Annotated[bool, typer.Option(help="Use cached results.")] = True) -> None:
     """Print ANSI representation of image."""
     start: float = perf_counter()
 
-    rprint("[italic grey78]Searching data in cache...[/]")
+    console.print("[italic grey78]Searching data in cache...[/]")
 
     frames: list[str] | object = cache.loadcache(file, additional=(cols, scale, char, color)) if caching else cache.sentinel
 
     if frames is cache.sentinel:
         if caching:
-            rprint("[italic bold orange1]No data, starting the processing...[/]")
+            console.print("[italic bold orange1]No data, starting the processing...[/]")
         else:
-            rprint("[italic bold yellow]Caching is disabled![/]")
-        with spinning_progress() as progress:
-            progress.add_task("Processing image...")
+            console.print("[italic bold yellow]Caching is disabled![/]")
+        with console.status("Processing image..."):
             im: Image.Image = process_image(file)
             with ProcessPoolExecutor() as tp:
                 framesf: list[Future] = [
@@ -104,14 +106,14 @@ def ansi(file: File,
                     cols, scale, char, color
                 ))
     else:
-        rprint("[italic bold cyan1]Data found![/]")
+        console.print("[italic bold cyan1]Data found![/]")
 
-    rprint(f"[italic grey78]Done in [bold cyan1]{perf_counter() - start:.2f}[/] seconds.[/]")
+    console.print(f"[italic grey78]Done in [bold cyan1]{perf_counter() - start:.2f}[/] seconds.[/]")
     if anim:
         i: int = 0
         frl: int = len(frames) - 1
         frheight: int = frames[0].count("\n") + 2
-        rprint(f"[italic grey78]There are [bold cyan1]{frheight}[/] rows.[/]")
+        console.print(f"[italic grey78]There are [bold cyan1]{frheight}[/] rows.[/]")
         try:
             while True:
                 print(frames[i])
@@ -120,7 +122,7 @@ def ansi(file: File,
                 i = 0 if i == frl else i + 1
         except KeyboardInterrupt:
             print(f"\x1b[{frheight}M\x1b[0m", end="")
-            rprint("[italic grey78]Done![/]")
+            console.print("[italic grey78]Done![/]")
     else:
         print(frames[0])
         print("\x1b[0m")
